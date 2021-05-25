60706c5810f223cd2ef5b39577eefc1553d6c298
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-05-25 13:54:45 +0100

    style the board and add in the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Board from './components/Board'
 
 function App() {
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
+
+      <Board />
+
     </div>
   );
 }
