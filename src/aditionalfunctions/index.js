f45a6ed4666eb0d3425f054a2b95104e57ed7495
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-05-27 15:27:05 +0100

    check win of game

---
+++ src/aditionalfunctions/index.js
@@ -0,0 +1,32 @@
+
+export const gameStatus = (board, pos) => {
+    const status = { result: 'UNKNOWN', types: [] }
+
+    //check column win
+    if (board[0][pos.y] === board[1][pos.y] && board[1][pos.y] === board[2][pos.y]) {
+        status.result = 'WIN'
+        status.types.push('col')
+    }
+
+    //check row win
+    if (board[pos.x][0] === board[pos.x][1] && board[pos.x][1] === board[pos.x][2]) {
+        status.result = 'WIN'
+        status.types.push('row')
+    }
+
+    //check diagno win
+    if (pos.x === pos.y && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        status.result = 'WIN'
+        status.types.push('diag')
+    }
+
+    //check other diagno win
+    if ((3 - pos.x + pos.y) === 1 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        status.result = 'WIN'
+        status.types.push('adiag')
+    }
+
+    return status
+}
+
+
