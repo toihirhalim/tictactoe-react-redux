9890a33a95532c14e1c1834f6803d55474372832
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-05-27 13:02:58 +0100

    update statistics reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+// game actions
 export const setGame = game => {
     return {
         type: 'SET',
@@ -21,15 +22,34 @@ export const resetGame = () => {
     }
 }
 
-export const addStatistic = stat => {
+// statistics actions
+export const setStatistic = (wins, losses, draws) => {
     return {
-        type: 'ADD',
-        stat
+        type: 'SET',
+        statatistics: { wins, losses, draws }
     }
 }
 
 export const clearStatatistics = () => {
     return {
         type: 'CLEAR',
+    }
+}
+
+export const wonGame = () => {
+    return {
+        type: 'WIN',
+    }
+}
+
+export const lostGame = () => {
+    return {
+        type: 'LOST',
+    }
+}
+
+export const drawGame = () => {
+    return {
+        type: 'DRAW',
     }
 }
--- src/reducers/statistics.js
+++ src/reducers/statistics.js
@@ -1,9 +1,21 @@
-const statisticsReducer = (state = [], action) => {
+const statistics = {
+    wins: 0,
+    losses: 0,
+    draws: 0
+}
+
+const statisticsReducer = (state = statistics, action) => {
     switch (action.type) {
-        case 'ADD':
-            return [...state, action.stat]
+        case 'WIN':
+            return { ...state, wins: state.wins + 1 }
+        case 'LOST':
+            return { ...state, losses: state.losses + 1 }
+        case 'DRAW':
+            return { ...state, draws: state.draws + 1 }
+        case 'SET':
+            return action.statistics
         case 'CLEAR':
-            return []
+            return statistics
         default:
             return state
     }
