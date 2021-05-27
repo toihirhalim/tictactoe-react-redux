99e4e25074281de6cde0e6e77390fe4302b97131
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-05-27 15:43:37 +0100

    stop the game if there is a win

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Case from './Case'
 import { useSelector, useDispatch } from 'react-redux'
 import { play } from '../actions'
+import { gameStatus } from '../aditionalfunctions'
 
 export default function Board() {
     const board = useSelector(state => state.game.board)
     const lastMove = useSelector(state => state.game.lastMove)
+    const movesCount = useSelector(state => state.game.movesCount)
     const dispatch = useDispatch()
+    const [status, setStatus] = useState({ result: 'UNKNOWN' })
 
     const playAtPosition = pos => {
+        if (status.result === 'WIN') return
+
         dispatch(play(pos))
     }
+
+    useEffect(() => {
+        if (movesCount >= 5) {
+            setStatus(gameStatus(board, lastMove))
+        }
+    }, [movesCount, lastMove, board])
 
     return (
         <div className="board">
