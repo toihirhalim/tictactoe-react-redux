05ee0ea127972e77cadb29627178552055eb629e
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-08-15 17:56:59 +0100

    check if the player is logged in on app load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 import Auth from './auth'
 import Game from './components/Game'
-import Header from './components/Header';
+import Header from './components/Header'
+import { useDispatch } from 'react-redux'
+import { login } from './actions'
+
 
 function App() {
+  const serverUri = process.env.REACT_APP_API_URL || 'https://server-tictactoe.herokuapp.com'
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    fetch(serverUri + '/isloggedin', { credentials: 'include' })
+      .then(res => {
+        if (res.ok)
+          res.json()
+            .then(data => {
+              dispatch(login(data.token))
+            })
+      })
+  }, [serverUri, dispatch])
   return (
     <div className="App">
       <Router>
