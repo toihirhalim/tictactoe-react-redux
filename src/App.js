9189d9b4002c9dc4e2cdfc2c8552dd80e1d09d16
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-08-20 17:11:20 +0100

    connect and disconnect socket on login/logout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 import Auth from './auth'
 import Game from './components/Game'
 import Header from './components/Header'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { login } from './actions'
+//import { socket, SocketContext } from './context/socket'
+import socketio from "socket.io-client";
 
+const SocketContext = React.createContext();
 
 function App() {
   const serverUri = process.env.REACT_APP_API_URL || 'https://server-tictactoe.herokuapp.com'
   const dispatch = useDispatch()
+  const isLogged = useSelector(state => state.auth.isLogged)
+  const [socket, setSocket] = useState(null)
 
   useEffect(() => {
     fetch(serverUri + '/isloggedin', { credentials: 'include' })
@@ -22,18 +27,30 @@ function App() {
             })
       })
   }, [serverUri, dispatch])
+
+  useEffect(() => {
+    if (isLogged) setSocket(socketio.connect(serverUri))
+    else setSocket(socket => {
+      if (socket) socket.disconnect()
+      return socket
+    })
+  }, [isLogged, serverUri])
+
   return (
     <div className="App">
-      <Router>
-        <Header />
 
-        <Game />
+      <SocketContext.Provider value={socket} >
+        <Router>
+          <Header />
 
-        <Route path={["/login", "/signup", "/logout"]}>
-          <Auth />
-        </Route>
+          <Game />
 
-      </Router>
+          <Route path={["/login", "/signup", "/logout"]}>
+            <Auth />
+          </Route>
+
+        </Router>
+      </SocketContext.Provider>
 
     </div>
   );
